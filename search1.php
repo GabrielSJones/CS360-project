a735e1f8f78c0f78580487b4357ec7d5b3a17810
Chandler Calkins <chandlerjaycalkins@gmail.com>
2022-04-17 21:40:49 -0700

    edited the php query file for type 1 search a bit

--- search1.php
+++ search1.php
@@ -1,6 +1,88 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="bootstrap.min.css">
+		<link rel="stylesheet" href="navbar.css">
+		
+		<style>
+			.bd-placeholder-img
+			{
+				font-size: 1.125rem;
+				text-anchor: middle;
+				-webkit-user-select: none;
+				-moz-user-select: none;
+				user-select: none;
+			}
+
+			@media (min-width: 768px)
+			{
+				.bd-placeholder-img-lg
+				{
+					font-size: 3.5rem;
+				}
+			}
+		</style>
+	</head>
+	<body>
+		<main>
+		<!-- Search dropdown and +Add button at the top -->
+			<nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+				<div class="container-fluid">
+					<div class="collapse navbar-collapse" id="navbarsExample03">
+						<ul class="navbar-nav me-auto mb-2 mb-sm-0">
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Search</a>
+								<ul class="dropdown-menu" aria-labelledby="dropdown03">
+									<li><a class="dropdown-item" href="search1.html">Type 1</a></li>
+									<li><a class="dropdown-item" href="search2.html">Type 2</a></li>
+									<li><a class="dropdown-item" href="search3.html">Type 3</a></li>
+								</ul>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="add.html">+Add</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</main>
+		<script src="bootstrap.bundle.min.js"></script>
+	</body>
+</html>
+
 <?php
 	
+	// gets the user inputs from the search bar
 	$name = $_POST['name'];
-	print "$name";
+	
+	// variables used to log into the mysql server
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "company_database";
+	
+	// connects to the mysql server and crashes if it can't connect
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$q = "SELECT * FROM phones WHERE Name = '$name'";
+	$result = $conn->query($q);
+	
+	if ($result->num_rows > 0)
+	{
+		while ($row = $result->fetch_assoc())
+		{
+			echo "id: " . $row["PhoneID"] . " - Screen Size: " . $row["ScreenSize"] . " - Name: " . $row["Name"] . " - Manufacturer: " . $row["Manufacturer"] . " - Refresh Rate: " . $row["RefreshRate"] . "<br>";
+		}
+	}
+	else
+	{
+		echo "No results";
+	}
+	
+	$conn->close();
 	
 ?>
