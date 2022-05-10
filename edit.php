a1cc2605032280a142620ab29471f14aca65a112
Chandler Calkins <chandlerjaycalkins@gmail.com>
2022-05-10 16:07:24 -0700

    edit.html is now fully functional
    NOTE: edit.php is not functional yet

---
+++ edit.php
@@ -0,0 +1,65 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="bootstrap.min.css">
+		<link rel="stylesheet" href="navbar.css">
+		<link rel="stylesheet" href="custom.css">
+		
+		<style>
+			.bd-placeholder-img
+			{
+				font-size: 1.125rem;
+				text-anchor: middle;
+				-webkit-user-select: none;
+				-moz-user-select: none;
+				user-select: none;
+			}
+
+			@media (min-width: 768px)
+			{
+				.bd-placeholder-img-lg
+				{
+					font-size: 3.5rem;
+				}
+			}
+		</style>
+	</head>
+	<body>
+		<main>
+		<!-- Search dropdown and +Add button at the top -->
+			<nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+				<div class="container-fluid">
+					<div class="collapse navbar-collapse" id="navbarsExample03">
+						<ul class="navbar-nav me-auto mb-2 mb-sm-0">
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Search</a>
+								<ul class="dropdown-menu" aria-labelledby="dropdown03">
+									<li><a class="dropdown-item" href="search1.html">Type 1</a></li>
+									<li><a class="dropdown-item" href="search2.html">Type 2</a></li>
+									<li><a class="dropdown-item" href="search3.html">Type 3</a></li>
+								</ul>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">+Add</a>
+								<ul class="dropdown-menu" aria-labelledby="dropdown03">
+									<li><a class="dropdown-item" href="addphones.html">Phones</a></li>
+									<li><a class="dropdown-item" href="addtvs.html">Televisions</a></li>
+									<li><a class="dropdown-item" href="addinternet.html">Internet</a></li>
+									<li><a class="dropdown-item" href="addplans.html">Plans</a></li>
+									<li><a class="dropdown-item" href="addcompanies.html">Companies</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</main>
+		<script src="bootstrap.bundle.min.js"></script>
+	</body>
+</html>
+
+<?php
+	
+	
+	
+?>
