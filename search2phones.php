7004eb8ba1be6c447ba6db4b399bded61d142bd4
Chandler Calkins <chandlerjaycalkins@gmail.com>
2022-05-11 13:14:22 -0700

    got search type 2 working for phones

--- search2phones.php
+++ search2phones.php
@@ -57,3 +57,110 @@
 		<script src="bootstrap.bundle.min.js"></script>
 	</body>
 </html>
+
+<?php
+	
+	// prints a table of results from a query
+	function printQueryResults($results, $attributes, $headers, $table)
+	{
+		if ($results->num_rows > 0)
+		{
+			echo "<center> <h1>Results</h1> </center>";
+			echo "<center>";
+			echo "<table>";
+			
+			echo "<tr>";
+			foreach ($headers as $header)
+			{
+				echo "<th>$header</th>";
+			}
+			echo "<th>Change</th>";
+			echo "</tr>";
+			
+			while ($row = $results->fetch_assoc())
+			{
+				echo "<tr>";
+				foreach ($attributes as $att)
+				{
+					echo "<td>" . $row["$att"] . "</td>";
+				}
+				echo "<td> <form action='edit.html' method='post'> <button name='iteminfo' value='$table+" . $row["$attributes[0]"] . "' type='submit' class='w-75 btn btn-primary'>Edit</button> </form>";
+				echo "<form action='delete.php' method='post'> <button name='iteminfo' value='$table+" . $row["$attributes[0]"] . "' type='submit' class='w-75 btn btn-danger'>Delete</button> </form> </td>";
+				echo "</tr>";
+			}
+			
+			echo "</table>";
+			echo "</center>";
+		}
+		else
+		{
+			echo "<center> <h1>No Results</h1> </center>";
+		}
+	}
+	
+	$screenSizeType = $_POST['ScreenSizeType'];
+	$screenSize = $_POST['ScreenSize'];
+	$refreshRateType = $_POST['RefreshRateType'];
+	$refreshRate = $_POST['RefreshRate'];
+	$manufacturer = $_POST['Manufacturer'];
+	$screenSize = $_POST['ScreenSize'];
+	
+	if ($screenSizeType != "any" and $screenSize == "")
+	{
+		echo "<center> <h1>Error: Please enter a value for Screen Size</h1> </center>";
+		die();
+	}
+	if ($refreshRateType != "any" and $refreshRate == "")
+	{
+		echo "<center> <h1>Error: Please enter a value for Refresh Rate</h1> </center>";
+		die();
+	}
+	
+	// variables used to log into the mysql server
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "company_database";
+	
+	// connects to the mysql server and crashes if it can't connect
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$query = "SELECT * FROM Phones";
+	if ($screenSizeType != "any" or $refreshRateType != "any" or $manufacturer != "")
+	{
+		$query .= " WHERE";
+		$first = TRUE;
+		if ($screenSizeType != "any")
+		{
+			$query .= " ScreenSize $screenSizeType '$screenSize'";
+			$first = FALSE;
+		}
+		if ($refreshRateType != "any")
+		{
+			if (!$first)
+			{
+				$query .= " AND";
+			}
+			$query .= " RefreshRate $refreshRateType '$refreshRate'";
+			$first = FALSE;
+		}
+		if ($manufacturer != "")
+		{
+			if (!$first)
+			{
+				$query .= " AND";
+			}
+			$query .= " LOWER(Manufacturer) = LOWER('$manufacturer')";
+		}
+	}
+	
+	$results = $conn->query($query);
+	$attributes = array("PhoneID", "Name", "ScreenSize", "Manufacturer", "RefreshRate");
+	$headers = array("ID", "Name", "Screen Size", "Manufacturer", "Refresh Rate");
+	printQueryResults($results, $attributes, $headers, "Phones");
+	
+?>
