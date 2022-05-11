143e50db0281b87e7a7940725c232c1dc2d3e7eb
Chandler Calkins <chandlerjaycalkins@gmail.com>
2022-05-11 13:54:55 -0700

    got search type 2 working for internet

--- search2internet.php
+++ search2internet.php
@@ -57,3 +57,124 @@
 		<script src="bootstrap.bundle.min.js"></script>
 	</body>
 </html>
+
+<?php
+	
+	// prints a table of results from a query
+	function printQueryResults($results, $attributes, $headers, $table)
+	{
+		if ($results->num_rows > 0)
+		{
+			echo "<center> <h1>Results</h1> </center>";
+			echo "<center>";
+			echo "<table>";
+			
+			echo "<tr>";
+			foreach ($headers as $header)
+			{
+				echo "<th>$header</th>";
+			}
+			echo "<th>Change</th>";
+			echo "</tr>";
+			
+			while ($row = $results->fetch_assoc())
+			{
+				echo "<tr>";
+				foreach ($attributes as $att)
+				{
+					echo "<td>" . $row["$att"] . "</td>";
+				}
+				echo "<td> <form action='edit.html' method='post'> <button name='iteminfo' value='$table+" . $row["$attributes[0]"] . "' type='submit' class='w-75 btn btn-primary'>Edit</button> </form>";
+				echo "<form action='delete.php' method='post'> <button name='iteminfo' value='$table+" . $row["$attributes[0]"] . "' type='submit' class='w-75 btn btn-danger'>Delete</button> </form> </td>";
+				echo "</tr>";
+			}
+			
+			echo "</table>";
+			echo "</center>";
+		}
+		else
+		{
+			echo "<center> <h1>No Results</h1> </center>";
+		}
+	}
+	
+	$downloadSpeedType = $_POST['DownloadSpeedType'];
+	$downloadSpeed = $_POST['DownloadSpeed'];
+	$uploadSpeedType = $_POST['UploadSpeedType'];
+	$uploadSpeed = $_POST['UploadSpeed'];
+	$dataCapType = $_POST['DataCapType'];
+	$dataCap = $_POST['DataCap'];
+	$internetType = $_POST['InternetType'];
+	
+	if ($downloadSpeedType != "any" and $downloadSpeed == "")
+	{
+		echo "<center> <h1>Error: Please enter a value for Download Speed</h1> </center>";
+		die();
+	}if ($uploadSpeedType != "any" and $uploadSpeed == "")
+	{
+		echo "<center> <h1>Error: Please enter a value for Upload Speed</h1> </center>";
+		die();
+	}if ($dataCapType != "any" and $dataCap == "")
+	{
+		echo "<center> <h1>Error: Please enter a value for Data Cap</h1> </center>";
+		die();
+	}
+	
+	// variables used to log into the mysql server
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "company_database";
+	
+	// connects to the mysql server and crashes if it can't connect
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$query = "SELECT * FROM Internet";
+	if ($downloadSpeedType != "any" or $uploadSpeedType != "any" or $dataCapType != "any" or $internetType != "any")
+	{
+		$query .= " WHERE";
+		$first = TRUE;
+		if ($downloadSpeedType != "any")
+		{
+			$query .= " DownloadSpeed $downloadSpeedType '$downloadSpeed'";
+			$first = FALSE;
+		}
+		if ($uploadSpeedType != "any")
+		{
+			if (!$first)
+			{
+				$query .= " AND";
+			}
+			$query .= " UploadSpeed $uploadSpeedType '$uploadSpeed'";
+			$first = FALSE;
+		}
+		if ($dataCapType != "any")
+		{
+			if (!$first)
+			{
+				$query .= " AND";
+			}
+			$query .= " DataCap $DataCapType '$dataCape'";
+			$first = FALSE;
+		}
+		if ($internetType != "any")
+		{
+			if (!$first)
+			{
+				$query .= " AND";
+			}
+			$query .= " InternetType = '$internetType'";
+			$first = FALSE;
+		}
+	}
+	
+	$results = $conn->query($query);
+	$attributes = array("InternetID", "Name", "DownloadSpeed", "UploadSpeed", "DataCap", "InternetType");
+	$headers = array("ID", "Name", "Download Speed", "UploadSpeed", "Data Cap", "Internet Type");
+	printQueryResults($results, $attributes, $headers, "Internet");
+	
+?>
